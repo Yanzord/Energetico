2e4f472ab520acf16b19d49606aa14e1619bef77
yanzord <yansantos0120@gmail.com>
2019-07-08 19:24:56 -0300

    Cria um método para limpar a tela

--- src/br/com/aceleradora/calculaimposto/app/AppPrincipal.java
+++ src/br/com/aceleradora/calculaimposto/app/AppPrincipal.java
@@ -16,33 +16,59 @@ public class AppPrincipal {
         String nomeCliente;
 
         while(opcaoInserida == 1 || opcaoInserida == 2) {
+
+            limparTela();
+
             System.out.println("Bem vindo ao programa x!");
             System.out.println("Digite 1 para inserir uma nota.");
             System.out.println("Digite 2 para ver o relatório de notas inseridas.");
             System.out.println("Digite qualquer outro caractere para sair.");
             System.out.println("Insira a opção desejada:");
+
             opcaoInserida = teclado.nextInt();
 
             switch (opcaoInserida) {
                 case 1: {
+                    limparTela();
+
                     System.out.println("Digite o nome do cliente:");
                     nomeCliente = teclado.nextLine();
+
                     System.out.println("Digite a quantidade de energeticos comprados:");
                     qtdEnergetico = teclado.nextInt();
 
+                    NotaFiscal nota = new NotaFiscal(nomeCliente, qtdEnergetico);
+                    notas.add(nota);
 
+                   limparTela();
 
+                    System.out.println("Nota adicionada com sucesso!");
+
+                    break;
                 }
-
                 case 2: {
 
+                   limparTela();
+
+                    mostrarRelatorio();
+
+                    break;
                 }
 
                 default: {
-                    System.exit(0);
+                    break;
                 }
             }
         }
     }
 
+    public static void limparTela() {
+        if (System.getProperty("os.name").contains("Windows")) {
+            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+        } else {
+            Runtime.getRuntime().exec("clear");
+        }
+
+    }
 }
+
