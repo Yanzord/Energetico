4995cd836c5919579ef19564f49f930cd8a75858
yanzord <yansantos0120@gmail.com>
2019-07-09 16:51:08 -0300

    Comentou as linhas do método limparTela

--- src/br/com/aceleradora/calculaimposto/app/AppPrincipal.java
+++ src/br/com/aceleradora/calculaimposto/app/AppPrincipal.java
@@ -1,4 +1,5 @@
 package br.com.aceleradora.calculaimposto.app;
+import java.io.IOException;
 import java.util.Scanner;
 
 import br.com.aceleradora.calculaimposto.classes.NotaFiscal;
@@ -9,27 +10,27 @@ public class AppPrincipal {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         Relatorios relatorios = new Relatorios();
-        int opcaoInserida = 1, qtdEnergetico;
+        int qtdEnergetico, opcaoInserida = 1;
         String nomeCliente;
 
         while(opcaoInserida == 1 || opcaoInserida == 2) {
 
-            limparTela();
+            //limparTela();
 
             System.out.println("Bem vindo ao programa x!");
             System.out.println("Digite 1 para inserir uma nota.");
             System.out.println("Digite 2 para ver o relatório de notas inseridas.");
-            System.out.println("Digite qualquer outro caractere para sair.");
+            System.out.println("Digite qualquer outro numero para sair.");
             System.out.println("Insira a opção desejada:");
 
             opcaoInserida = teclado.nextInt();
 
             switch (opcaoInserida) {
                 case 1: {
-                    limparTela();
+                    //limparTela();
 
                     System.out.println("Digite o nome do cliente:");
-                    nomeCliente = teclado.nextLine();
+                    nomeCliente = teclado.next();
 
                     System.out.println("Digite a quantidade de energeticos comprados:");
                     qtdEnergetico = teclado.nextInt();
@@ -37,7 +38,7 @@ public class AppPrincipal {
                     NotaFiscal nota = new NotaFiscal(nomeCliente, qtdEnergetico);
                     relatorios.gerarRelatorio(nota);
 
-                    limparTela();
+                    //limparTela();
 
                     System.out.println("Nota adicionada com sucesso!");
 
@@ -45,7 +46,7 @@ public class AppPrincipal {
                 }
                 case 2: {
 
-                    limparTela();
+                    //limparTela();
 
                     relatorios.mostrarRelatorio();
 
@@ -59,13 +60,13 @@ public class AppPrincipal {
         }
     }
 
-    public static void limparTela() {
+    /*public static void limparTela() throws IOException {
         if (System.getProperty("os.name").contains("Windows")) {
             new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
         } else {
             Runtime.getRuntime().exec("clear");
         }
 
-    }
+    }*/
 }
 
