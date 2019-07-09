5f80ccb4d7c3972ae469303ea79a39b222f20abe
yanzord <yansantos0120@gmail.com>
2019-07-09 16:52:26 -0300

    Criou o método para mostrar o relatório de notas

--- src/br/com/aceleradora/calculaimposto/classes/Relatorios.java
+++ src/br/com/aceleradora/calculaimposto/classes/Relatorios.java
@@ -12,9 +12,13 @@ public class Relatorios {
     }
 
     public void mostrarRelatorio(){
-        int i;
-        for (i = 0; i < notas.size(); i++ ){
-            System.out.println(notas.get(i));
+        double totalImpostos = 0, totalMercadorias = 0, totalGeral = 0;
+        for (NotaFiscal i : notas){
+            System.out.println("Cliente: " +i.getNomeCliente()+"\nICMS: R$"+i.getIcms()+"; IPI: R$"+i.getIpi()+"; PIS: R$"+i.getPis()+"; COFINS: "+i.getCofins()+"; Total: R$"+i.getTotalNota()+"\n");
+            totalImpostos += i.getIcms() + i.getIpi() + i.getPis() + i.getCofins();
+            totalMercadorias += i.getQtdEnergetico() * i.getValorEnergetico();
+            totalGeral += i.getTotalNota();
         }
+        System.out.println("Total Impostos: R$"+totalImpostos+"\nTotal Mercadorias: R$"+totalMercadorias+"\nTotal Geral: R$"+totalGeral);
     }
 }
