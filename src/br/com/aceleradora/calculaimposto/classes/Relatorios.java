9c859cd179c94b5d594668e75e02d87041f56d6d
yanzord <yansantos0120@gmail.com>
2019-07-09 17:10:53 -0300

    Alterou o nome da variável local i para nota no método mostrarRelatorio

--- src/br/com/aceleradora/calculaimposto/classes/Relatorios.java
+++ src/br/com/aceleradora/calculaimposto/classes/Relatorios.java
@@ -13,11 +13,11 @@ public class Relatorios {
 
     public void mostrarRelatorio(){
         double totalImpostos = 0, totalMercadorias = 0, totalGeral = 0;
-        for (NotaFiscal i : notas){
-            System.out.println("Cliente: " +i.getNomeCliente()+"\nICMS: R$"+i.getIcms()+"; IPI: R$"+i.getIpi()+"; PIS: R$"+i.getPis()+"; COFINS: "+i.getCofins()+"; Total: R$"+i.getTotalNota()+"\n");
-            totalImpostos += i.getIcms() + i.getIpi() + i.getPis() + i.getCofins();
-            totalMercadorias += i.getQtdEnergetico() * i.getValorEnergetico();
-            totalGeral += i.getTotalNota();
+        for (NotaFiscal nota : notas){
+            System.out.println("Cliente: " +nota.getNomeCliente()+"\nICMS: R$"+nota.getIcms()+"; IPI: R$"+nota.getIpi()+"; PIS: R$"+nota.getPis()+"; COFINS: "+nota.getCofins()+"; Total: R$"+nota.getTotalNota()+"\n");
+            totalImpostos += nota.getIcms() + nota.getIpi() + nota.getPis() + nota.getCofins();
+            totalMercadorias += nota.getQtdEnergetico() * nota.getValorEnergetico();
+            totalGeral += nota.getTotalNota();
         }
         System.out.println("Total Impostos: R$"+totalImpostos+"\nTotal Mercadorias: R$"+totalMercadorias+"\nTotal Geral: R$"+totalGeral);
     }
