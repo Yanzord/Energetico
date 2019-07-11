855832c54508d3e4970e65b25f63865517ece8e6
yanzord <yansantos0120@gmail.com>
2019-07-11 19:53:33 -0300

    Adicionou um if/else para caso o cliente possua desconto

--- src/br/com/aceleradora/calculaimposto/classes/Relatorios.java
+++ src/br/com/aceleradora/calculaimposto/classes/Relatorios.java
@@ -14,10 +14,16 @@ public class Relatorios {
     public void mostrarRelatorio(){
         double totalImpostos = 0, totalMercadorias = 0, totalGeral = 0;
         for (NotaFiscal nota : notas){
-            System.out.println("Cliente: " +nota.getNomeCliente()+"\nICMS: R$"+nota.getIcms()+"; IPI: R$"+nota.getIpi()+"; PIS: R$"+nota.getPis()+"; COFINS: "+nota.getCofins()+"; Total: R$"+nota.getTotalNota()+"\n");
+            if (nota.getDescontoNota() != 0.00){
+                System.out.println("Cliente ganhou desconto de 15%!");
+                System.out.println("Cliente: " +nota.getNomeCliente()+"\nICMS: R$"+nota.getIcms()+"; IPI: R$"+nota.getIpi()+"; PIS: R$"+nota.getPis()+"; COFINS: "+nota.getCofins()+"; Total com desconto: R$"+nota.getTotalNotaComImposto()+"\n");
+            }else {
+                System.out.println("Cliente: " +nota.getNomeCliente()+"\nICMS: R$"+nota.getIcms()+"; IPI: R$"+nota.getIpi()+"; PIS: R$"+nota.getPis()+"; COFINS: "+nota.getCofins()+"; Total: R$"+nota.getTotalNotaComImposto()+"\n");
+            }
+
             totalImpostos += nota.getIcms() + nota.getIpi() + nota.getPis() + nota.getCofins();
-            totalMercadorias += nota.getQtdEnergetico() * nota.getValorEnergetico();
-            totalGeral += nota.getTotalNota();
+            totalMercadorias += nota.getTotalNotaSemImposto();
+            totalGeral += nota.getTotalNotaComImposto();
         }
         System.out.println("Total Impostos: R$"+totalImpostos+"\nTotal Mercadorias: R$"+totalMercadorias+"\nTotal Geral: R$"+totalGeral);
     }
